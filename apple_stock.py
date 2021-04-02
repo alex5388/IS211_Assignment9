b8da3b140e9a765fac011b0527806ed57238f950
alex5388 <alexmiller5388@gmail.com>
2021-04-02 18:19:28 -0400

    Update apple_stock.py

--- apple_stock.py
+++ apple_stock.py
@@ -4,10 +4,8 @@ import requests
 r = requests.get('https://finance.yahoo.com/quote/AAPL/history?p=AAPL').text
 soup = BeautifulSoup(r, 'lxml')
 
-for match in soup.find_all('tr', {'class':'BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)'}):
-
-
-    date = match.text[:12]
-    price = match.text[30:36]
+for match in soup.find_all('tr', {'class':'BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)'}):  #locate historical pricing data in html
+    date = match.text[:12]          #parse out date
+    price = match.text[30:36]       #parse out closing price
     print(f'{date}    {price}')
 
