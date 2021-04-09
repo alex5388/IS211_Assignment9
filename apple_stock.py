b03df642f2f24776f3e0d681bcb28edb19eec165
alex5388 <alexmiller5388@gmail.com>
2021-04-09 16:54:08 -0400

    Add files via upload

--- apple_stock.py
+++ apple_stock.py
@@ -1,11 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get('https://finance.yahoo.com/quote/AAPL/history?p=AAPL').text
-soup = BeautifulSoup(r, 'lxml')
 
-for match in soup.find_all('tr', {'class':'BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)'}):  #locate historical pricing data in html
-    date = match.text[:12]          #parse out date
-    price = match.text[30:36]       #parse out closing price
-    print(f'{date}    {price}')
+def main():
+    r = requests.get('https://finance.yahoo.com/quote/AAPL/history?p=AAPL').text   #get the website
+    soup = BeautifulSoup(r, 'lxml')
 
+    print('Date            AAPL Closing Price')
+    for match in soup.find_all('tr', {'class':'BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)'}):  #locate historical pricing data 
+        date = match.text[:12]          #parse out date
+        price = match.text[30:36]       #parse out closing price
+
+        print(f'{date}    {price}')
+
+if __name__=="__main__":
+    main()
