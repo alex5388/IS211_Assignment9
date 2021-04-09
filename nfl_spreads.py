364e77da8aa6bfcdd73b8f7fd444ed1a3a6acb54
alex5388 <alexmiller5388@gmail.com>
2021-04-09 16:37:15 -0400

    Update nfl_spreads.py

--- nfl_spreads.py
+++ nfl_spreads.py
@@ -1,8 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get('http://www.footballlocks.com/nfl_point_spreads.shtml').text
-soup = BeautifulSoup(r, 'lxml')
+def main():
+  r = requests.get('http://www.footballlocks.com/nfl_point_spreads.shtml').text
+  soup = BeautifulSoup(r, 'lxml')
 
-match = soup.find('span', {'style':'font-size:10.0pt;font-family:verdana'}).find('table')
-print(match.text[143:169])
+  match = soup.find('span', {'style':'font-size:10.0pt;font-family:verdana'}).find('table')
+  print(match.text[143:169])
+
+if __name__=="__main__":
+  main()
